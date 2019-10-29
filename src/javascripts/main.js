671a9e7da6e3f3eef6457d2d27475864ba8465e7
Ashley Claiborne <languageisaliquid@gmail.com>
2019-10-28 19:53:31 -0500

    added farmers

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,9 +1,11 @@
 import 'bootstrap';
 import '../styles/main.scss';
 import cows from './components/cowList/cowList';
+import farmers from './components/farmerList/farmerList';
 
 const init = () => {
   cows.buildCows();
+  farmers.buildfarmers();
 };
 
 init();
